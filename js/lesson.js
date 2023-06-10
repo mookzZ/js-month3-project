9654df6b3a3d953e409690b8b4b0cfdbb7cb47d5
acided <homejet2007@gmail.com>
2023-06-10 22:10:08 +0600

    норм версия

--- js/lesson.js
+++ js/lesson.js
@@ -1,16 +1,16 @@
 // REG EXP
-const phoneInput = document.querySelector('#phoneInput')
-const phoneCheck = document.querySelector('#phoneCheck')
-const phoneResult = document.querySelector('.phoneResult')
+const phoneInput = document.querySelector('#phone_input')
+const phoneButton = document.querySelector('#phone_button')
+const phoneResult = document.querySelector('#phone_result')
 
 const regExp = /^\+996 \d{3} \d{2}-\d{2}-\d{2}$/
 
-phoneCheck.onclick = () => {
+phoneButton.onclick = () => {
     if (regExp.test(phoneInput.value)) {
-        phoneResult.innerHTML = 'OK'
+        phoneResult.innerHTML = 'YOUR NUMBER IS VALID!'
         phoneResult.style.color = 'green'
     } else {
-        phoneResult.innerHTML = 'NOT OK'
+        phoneResult.innerHTML = 'YOUR NUMBER IS NOT VALID'
         phoneResult.style.color = 'red'
     }
 }
