a3cf2912f7d9036fa95efb9d605e22f8b40bae18
acided <homejet2007@gmail.com>
2023-06-10 23:37:15 +0600

    part 1 hw2

--- js/home_works.js
+++ js/home_works.js
@@ -18,20 +18,48 @@ check.onclick = () => {
 }
 
 //MOVING SQUARE//
-const redSquare = document.querySelector('.child_block')
+const redSquare = document.querySelector('.child_block');
 let positionX = 0
 let positionY = 0
 
-function moveBlock() {
-    if (positionX < 446) {
+const moveBlock = () => {
+    if (positionX < 446 && positionY === 0) {
         positionX += 1
         redSquare.style.left = positionX + 'px'
-        setTimeout(moveBlock, 1)
-    } else if (positionY < 446) {
+    } else if (positionX === 446 && positionY < 446) {
         positionY += 1
         redSquare.style.top = positionY + 'px'
-        setTimeout(moveBlock, 1)
+    } else if (positionX > 0 && positionY === 446) {
+        positionX -= 1
+        redSquare.style.left = positionX + 'px'
+    } else if (positionX === 0 && positionY > 0) {
+        positionY -= 1
+        redSquare.style.top = positionY + 'px'
     }
+    setTimeout(moveBlock, 5)
 }
 
 moveBlock()
+
+// STOPWATCH //
+
+const stopwatchNumber = document.querySelector('.stopwatch_number')
+const stopwatchStart = document.querySelector('.stopwatch_start')
+const stopwatchResume = document.querySelector('.stopwatch_resume')
+const stopwatchStop = document.querySelector('.stopwatch_stop')
+const stopwatchClear = document.querySelector('.stopwatch_clear')
+
+let number = 0
+
+const start = () => {
+    number += 1
+    stopwatchNumber.innerHTML = `${number} сек.`
+}
+
+const stop = () => {
+    clearInterval()
+}
+
+stopwatchStart.onclick = function (startInterval) {
+    setInterval(start, 1000)
+}
